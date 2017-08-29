c1e214ab5baaffcbbd19af0752c8ac59e157a433
davvid13 <davvid19996@gmail.com>
2017-08-30 01:25:50 +0800

    2nd activity = make a an addition calculator = done

--- Prototype/Visual Studio/Tutorial/Tutorial/Form1.Designer.vb
+++ Prototype/Visual Studio/Tutorial/Tutorial/Form1.Designer.vb
@@ -27,6 +27,12 @@ Partial Class Form1
         Me.btnOk = New System.Windows.Forms.Button()
         Me.btnMessage = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtAddVal1 = New System.Windows.Forms.TextBox()
+        Me.btnAddValues = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtAddVal2 = New System.Windows.Forms.TextBox()
+        Me.lblAddEqual = New System.Windows.Forms.Label()
+        Me.txtSumAnswer = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'lblName
@@ -72,11 +78,65 @@ Partial Class Form1
         Me.btnExit.Text = "E&xit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
+        'txtAddVal1
+        '
+        Me.txtAddVal1.Location = New System.Drawing.Point(12, 78)
+        Me.txtAddVal1.Name = "txtAddVal1"
+        Me.txtAddVal1.Size = New System.Drawing.Size(50, 20)
+        Me.txtAddVal1.TabIndex = 5
+        '
+        'btnAddValues
+        '
+        Me.btnAddValues.Location = New System.Drawing.Point(68, 76)
+        Me.btnAddValues.Name = "btnAddValues"
+        Me.btnAddValues.Size = New System.Drawing.Size(23, 23)
+        Me.btnAddValues.TabIndex = 6
+        Me.btnAddValues.Text = "+"
+        Me.btnAddValues.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(131, 119)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(23, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "+"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtAddVal2
+        '
+        Me.txtAddVal2.Location = New System.Drawing.Point(99, 78)
+        Me.txtAddVal2.Name = "txtAddVal2"
+        Me.txtAddVal2.Size = New System.Drawing.Size(55, 20)
+        Me.txtAddVal2.TabIndex = 8
+        '
+        'lblAddEqual
+        '
+        Me.lblAddEqual.AutoSize = True
+        Me.lblAddEqual.Location = New System.Drawing.Point(160, 81)
+        Me.lblAddEqual.Name = "lblAddEqual"
+        Me.lblAddEqual.Size = New System.Drawing.Size(13, 13)
+        Me.lblAddEqual.TabIndex = 9
+        Me.lblAddEqual.Text = "="
+        '
+        'txtSumAnswer
+        '
+        Me.txtSumAnswer.Location = New System.Drawing.Point(179, 78)
+        Me.txtSumAnswer.Name = "txtSumAnswer"
+        Me.txtSumAnswer.Size = New System.Drawing.Size(55, 20)
+        Me.txtSumAnswer.TabIndex = 10
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.txtSumAnswer)
+        Me.Controls.Add(Me.lblAddEqual)
+        Me.Controls.Add(Me.txtAddVal2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnAddValues)
+        Me.Controls.Add(Me.txtAddVal1)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnMessage)
         Me.Controls.Add(Me.btnOk)
@@ -94,4 +154,10 @@ Partial Class Form1
     Friend WithEvents btnOk As Button
     Friend WithEvents btnMessage As Button
     Friend WithEvents btnExit As Button
+    Friend WithEvents txtAddVal1 As TextBox
+    Friend WithEvents btnAddValues As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtAddVal2 As TextBox
+    Friend WithEvents lblAddEqual As Label
+    Friend WithEvents txtSumAnswer As TextBox
 End Class
--- Prototype/Visual Studio/Tutorial/Tutorial/Form1.vb
+++ Prototype/Visual Studio/Tutorial/Tutorial/Form1.vb
@@ -19,4 +19,25 @@
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles btnAddValues.Click
+
+        Try
+            Dim firstNum As Integer = CInt(txtAddVal1.Text)
+            Dim secondNum As Integer = CInt(txtAddVal2.Text)
+            Dim sum = firstNum + secondNum
+
+            txtSumAnswer.Text = CStr(sum)
+
+        Catch ex As System.InvalidCastException
+            MessageBox.Show("Please enter numbers to sum", "Error")
+
+            Console.WriteLine("An Error occured")
+
+        Catch ex As Exception
+            MessageBox.Show("An unknown error occured", "Error")
+
+        End Try
+
+    End Sub
 End Class
