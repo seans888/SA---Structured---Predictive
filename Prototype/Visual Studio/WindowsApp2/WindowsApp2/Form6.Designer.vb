643861f488d2b97fcee3c286d8bfddae946f6b2f
davvid13 <davvid19996@gmail.com>
2017-09-01 22:25:08 +0800

    Added form 7 & 8 Form 7 contains the negative comments of rank 1 Form 8 contains the negative comments of rank 2

--- Prototype/Visual Studio/WindowsApp2/WindowsApp2/Form6.Designer.vb
+++ Prototype/Visual Studio/WindowsApp2/WindowsApp2/Form6.Designer.vb
@@ -30,7 +30,26 @@ Partial Class Form6
         Me.btnCpr5 = New System.Windows.Forms.Button()
         Me.btnMcr5 = New System.Windows.Forms.Button()
         Me.btnHome5 = New System.Windows.Forms.Button()
+        Me.lblRank6 = New System.Windows.Forms.Label()
+        Me.lblConcern6 = New System.Windows.Forms.Label()
+        Me.lblPercentage6 = New System.Windows.Forms.Label()
+        Me.lblNum1_6 = New System.Windows.Forms.Label()
+        Me.lblConcern1_6 = New System.Windows.Forms.Label()
+        Me.lblPercentage1_6 = New System.Windows.Forms.Label()
+        Me.lblRank2_6 = New System.Windows.Forms.Label()
+        Me.lblConcern2_6 = New System.Windows.Forms.Label()
+        Me.lblPercentage2_6 = New System.Windows.Forms.Label()
+        Me.lblRank3_6 = New System.Windows.Forms.Label()
+        Me.lblConcern3_6 = New System.Windows.Forms.Label()
+        Me.lblPercentage3_6 = New System.Windows.Forms.Label()
+        Me.lblRank4_6 = New System.Windows.Forms.Label()
+        Me.lblRank5_6 = New System.Windows.Forms.Label()
+        Me.lblConcern4_6 = New System.Windows.Forms.Label()
+        Me.lblPercentage4_6 = New System.Windows.Forms.Label()
+        Me.lblConcern5_6 = New System.Windows.Forms.Label()
+        Me.lblPercentage5_6 = New System.Windows.Forms.Label()
         CType(Me.picSM2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TableLayoutPanel1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -46,16 +65,40 @@ Partial Class Form6
         '
         'TableLayoutPanel1
         '
+        Me.TableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.TableLayoutPanel1.ColumnCount = 3
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.19939!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 57.70393!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.09668!))
+        Me.TableLayoutPanel1.Controls.Add(Me.lblRank5_6, 0, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblPercentage2_6, 2, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblConcern2_6, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblRank2_6, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblPercentage1_6, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblPercentage6, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblRank6, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblConcern6, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblNum1_6, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblConcern1_6, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblRank3_6, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblConcern3_6, 1, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblPercentage3_6, 2, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblRank4_6, 0, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblConcern4_6, 1, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblPercentage4_6, 2, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblConcern5_6, 1, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.lblPercentage5_6, 2, 5)
+        Me.TableLayoutPanel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(33, 145)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
-        Me.TableLayoutPanel1.RowCount = 2
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(330, 100)
+        Me.TableLayoutPanel1.RowCount = 6
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.2948!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.91908!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.49711!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.76301!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.76301!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.60694!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(330, 173)
         Me.TableLayoutPanel1.TabIndex = 10
         '
         'PictureBox1
@@ -109,6 +152,214 @@ Partial Class Form6
         Me.btnHome5.Text = "Home"
         Me.btnHome5.UseVisualStyleBackColor = False
         '
+        'lblRank6
+        '
+        Me.lblRank6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblRank6.AutoSize = True
+        Me.lblRank6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRank6.Location = New System.Drawing.Point(3, 4)
+        Me.lblRank6.Name = "lblRank6"
+        Me.lblRank6.Size = New System.Drawing.Size(40, 15)
+        Me.lblRank6.TabIndex = 0
+        Me.lblRank6.Text = "Rank"
+        '
+        'lblConcern6
+        '
+        Me.lblConcern6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblConcern6.AutoSize = True
+        Me.lblConcern6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConcern6.Location = New System.Drawing.Point(111, 4)
+        Me.lblConcern6.Name = "lblConcern6"
+        Me.lblConcern6.Size = New System.Drawing.Size(60, 15)
+        Me.lblConcern6.TabIndex = 1
+        Me.lblConcern6.Text = "Concern"
+        '
+        'lblPercentage6
+        '
+        Me.lblPercentage6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblPercentage6.AutoSize = True
+        Me.lblPercentage6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPercentage6.Location = New System.Drawing.Point(243, 4)
+        Me.lblPercentage6.Name = "lblPercentage6"
+        Me.lblPercentage6.Size = New System.Drawing.Size(80, 15)
+        Me.lblPercentage6.TabIndex = 2
+        Me.lblPercentage6.Text = "Percentage"
+        '
+        'lblNum1_6
+        '
+        Me.lblNum1_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblNum1_6.AutoSize = True
+        Me.lblNum1_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum1_6.Location = New System.Drawing.Point(14, 28)
+        Me.lblNum1_6.Name = "lblNum1_6"
+        Me.lblNum1_6.Size = New System.Drawing.Size(18, 20)
+        Me.lblNum1_6.TabIndex = 3
+        Me.lblNum1_6.Text = "1"
+        Me.lblNum1_6.UseWaitCursor = True
+        '
+        'lblConcern1_6
+        '
+        Me.lblConcern1_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblConcern1_6.AutoSize = True
+        Me.lblConcern1_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConcern1_6.Location = New System.Drawing.Point(118, 28)
+        Me.lblConcern1_6.Name = "lblConcern1_6"
+        Me.lblConcern1_6.Size = New System.Drawing.Size(46, 20)
+        Me.lblConcern1_6.TabIndex = 4
+        Me.lblConcern1_6.Text = "Food"
+        '
+        'lblPercentage1_6
+        '
+        Me.lblPercentage1_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblPercentage1_6.AutoSize = True
+        Me.lblPercentage1_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPercentage1_6.Location = New System.Drawing.Point(262, 28)
+        Me.lblPercentage1_6.Name = "lblPercentage1_6"
+        Me.lblPercentage1_6.Size = New System.Drawing.Size(41, 20)
+        Me.lblPercentage1_6.TabIndex = 5
+        Me.lblPercentage1_6.Text = "30%"
+        Me.lblPercentage1_6.UseWaitCursor = True
+        '
+        'lblRank2_6
+        '
+        Me.lblRank2_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblRank2_6.AutoSize = True
+        Me.lblRank2_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRank2_6.Location = New System.Drawing.Point(14, 60)
+        Me.lblRank2_6.Name = "lblRank2_6"
+        Me.lblRank2_6.Size = New System.Drawing.Size(18, 20)
+        Me.lblRank2_6.TabIndex = 6
+        Me.lblRank2_6.Text = "2"
+        Me.lblRank2_6.UseWaitCursor = True
+        '
+        'lblConcern2_6
+        '
+        Me.lblConcern2_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblConcern2_6.AutoSize = True
+        Me.lblConcern2_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConcern2_6.Location = New System.Drawing.Point(84, 60)
+        Me.lblConcern2_6.Name = "lblConcern2_6"
+        Me.lblConcern2_6.Size = New System.Drawing.Size(113, 20)
+        Me.lblConcern2_6.TabIndex = 7
+        Me.lblConcern2_6.Text = "Service Quality"
+        '
+        'lblPercentage2_6
+        '
+        Me.lblPercentage2_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblPercentage2_6.AutoSize = True
+        Me.lblPercentage2_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPercentage2_6.Location = New System.Drawing.Point(262, 60)
+        Me.lblPercentage2_6.Name = "lblPercentage2_6"
+        Me.lblPercentage2_6.Size = New System.Drawing.Size(41, 20)
+        Me.lblPercentage2_6.TabIndex = 8
+        Me.lblPercentage2_6.Text = "25%"
+        Me.lblPercentage2_6.UseWaitCursor = True
+        '
+        'lblRank3_6
+        '
+        Me.lblRank3_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblRank3_6.AutoSize = True
+        Me.lblRank3_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRank3_6.Location = New System.Drawing.Point(14, 90)
+        Me.lblRank3_6.Name = "lblRank3_6"
+        Me.lblRank3_6.Size = New System.Drawing.Size(18, 20)
+        Me.lblRank3_6.TabIndex = 9
+        Me.lblRank3_6.Text = "3"
+        Me.lblRank3_6.UseWaitCursor = True
+        '
+        'lblConcern3_6
+        '
+        Me.lblConcern3_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblConcern3_6.AutoSize = True
+        Me.lblConcern3_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConcern3_6.Location = New System.Drawing.Point(89, 90)
+        Me.lblConcern3_6.Name = "lblConcern3_6"
+        Me.lblConcern3_6.Size = New System.Drawing.Size(104, 20)
+        Me.lblConcern3_6.TabIndex = 10
+        Me.lblConcern3_6.Text = "Room Quality"
+        '
+        'lblPercentage3_6
+        '
+        Me.lblPercentage3_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblPercentage3_6.AutoSize = True
+        Me.lblPercentage3_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPercentage3_6.Location = New System.Drawing.Point(262, 90)
+        Me.lblPercentage3_6.Name = "lblPercentage3_6"
+        Me.lblPercentage3_6.Size = New System.Drawing.Size(41, 20)
+        Me.lblPercentage3_6.TabIndex = 11
+        Me.lblPercentage3_6.Text = "15%"
+        Me.lblPercentage3_6.UseWaitCursor = True
+        '
+        'lblRank4_6
+        '
+        Me.lblRank4_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblRank4_6.AutoSize = True
+        Me.lblRank4_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRank4_6.Location = New System.Drawing.Point(14, 119)
+        Me.lblRank4_6.Name = "lblRank4_6"
+        Me.lblRank4_6.Size = New System.Drawing.Size(18, 20)
+        Me.lblRank4_6.TabIndex = 12
+        Me.lblRank4_6.Text = "4"
+        Me.lblRank4_6.UseWaitCursor = True
+        '
+        'lblRank5_6
+        '
+        Me.lblRank5_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblRank5_6.AutoSize = True
+        Me.lblRank5_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRank5_6.Location = New System.Drawing.Point(14, 148)
+        Me.lblRank5_6.Name = "lblRank5_6"
+        Me.lblRank5_6.Size = New System.Drawing.Size(18, 20)
+        Me.lblRank5_6.TabIndex = 13
+        Me.lblRank5_6.Text = "5"
+        Me.lblRank5_6.UseWaitCursor = True
+        '
+        'lblConcern4_6
+        '
+        Me.lblConcern4_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblConcern4_6.AutoSize = True
+        Me.lblConcern4_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConcern4_6.Location = New System.Drawing.Point(80, 119)
+        Me.lblConcern4_6.Name = "lblConcern4_6"
+        Me.lblConcern4_6.Size = New System.Drawing.Size(121, 20)
+        Me.lblConcern4_6.TabIndex = 14
+        Me.lblConcern4_6.Text = "Comfort Rooms"
+        '
+        'lblPercentage4_6
+        '
+        Me.lblPercentage4_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblPercentage4_6.AutoSize = True
+        Me.lblPercentage4_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPercentage4_6.Location = New System.Drawing.Point(262, 119)
+        Me.lblPercentage4_6.Name = "lblPercentage4_6"
+        Me.lblPercentage4_6.Size = New System.Drawing.Size(41, 20)
+        Me.lblPercentage4_6.TabIndex = 15
+        Me.lblPercentage4_6.Text = "10%"
+        Me.lblPercentage4_6.UseWaitCursor = True
+        '
+        'lblConcern5_6
+        '
+        Me.lblConcern5_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblConcern5_6.AutoSize = True
+        Me.lblConcern5_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConcern5_6.Location = New System.Drawing.Point(58, 148)
+        Me.lblConcern5_6.Name = "lblConcern5_6"
+        Me.lblConcern5_6.Size = New System.Drawing.Size(165, 20)
+        Me.lblConcern5_6.TabIndex = 16
+        Me.lblConcern5_6.Text = "Recreational Activities"
+        '
+        'lblPercentage5_6
+        '
+        Me.lblPercentage5_6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblPercentage5_6.AutoSize = True
+        Me.lblPercentage5_6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPercentage5_6.Location = New System.Drawing.Point(267, 148)
+        Me.lblPercentage5_6.Name = "lblPercentage5_6"
+        Me.lblPercentage5_6.Size = New System.Drawing.Size(32, 20)
+        Me.lblPercentage5_6.TabIndex = 17
+        Me.lblPercentage5_6.Text = "8%"
+        Me.lblPercentage5_6.UseWaitCursor = True
+        '
         'Form6
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -124,6 +375,8 @@ Partial Class Form6
         Me.Name = "Form6"
         Me.Text = " "
         CType(Me.picSM2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
@@ -136,4 +389,22 @@ Partial Class Form6
     Friend WithEvents btnCpr5 As Button
     Friend WithEvents btnMcr5 As Button
     Friend WithEvents btnHome5 As Button
+    Friend WithEvents lblPercentage6 As Label
+    Friend WithEvents lblRank6 As Label
+    Friend WithEvents lblConcern6 As Label
+    Friend WithEvents lblRank2_6 As Label
+    Friend WithEvents lblPercentage1_6 As Label
+    Friend WithEvents lblNum1_6 As Label
+    Friend WithEvents lblConcern1_6 As Label
+    Friend WithEvents lblRank5_6 As Label
+    Friend WithEvents lblPercentage2_6 As Label
+    Friend WithEvents lblConcern2_6 As Label
+    Friend WithEvents lblRank3_6 As Label
+    Friend WithEvents lblConcern3_6 As Label
+    Friend WithEvents lblPercentage3_6 As Label
+    Friend WithEvents lblRank4_6 As Label
+    Friend WithEvents lblConcern4_6 As Label
+    Friend WithEvents lblPercentage4_6 As Label
+    Friend WithEvents lblConcern5_6 As Label
+    Friend WithEvents lblPercentage5_6 As Label
 End Class
--- Prototype/Visual Studio/WindowsApp2/WindowsApp2/Form6.vb
+++ Prototype/Visual Studio/WindowsApp2/WindowsApp2/Form6.vb
@@ -2,4 +2,22 @@
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
 
     End Sub
+
+    Private Sub Form6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblPercentage1_6_Click(sender As Object, e As EventArgs) Handles lblPercentage1_6.Click
+
+    End Sub
+
+    Private Sub lblConcern1_6_Click(sender As Object, e As EventArgs) Handles lblConcern1_6.Click
+        My.Forms.Form7.Text = Now.ToString
+        My.Forms.Form7.Show()
+    End Sub
+
+    Private Sub lblConcern2_6_Click(sender As Object, e As EventArgs) Handles lblConcern2_6.Click
+        My.Forms.Form8.Text = Now.ToString
+        My.Forms.Form8.Show()
+    End Sub
 End Class
